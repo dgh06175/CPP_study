436bcff9456a1186b623b00cea8f16696c272d2f
dgh06175 <dgh06175@gmail.com>
2023-10-13 14:05:19 +0900

    231013_실습코드

---
+++ Cpp_lecture/circle/cal.h
@@ -0,0 +1,14 @@
+class Adder
+{ // 덧셈 모듈 클래스
+    int op1, op2;
+
+public:
+    Adder(int a, int b);
+    int process();
+};
+
+class Calculator
+{ // 계산기 클래스
+public:
+    void run();
+};
---
+++ Cpp_lecture/circle/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#include "cal.cpp"
+
+int main()
+{
+    Calculator calc; // calc 객체 생성
+    calc.run();      // 계산기 시작
+}
