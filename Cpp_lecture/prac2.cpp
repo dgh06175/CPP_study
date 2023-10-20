3d57248c1334912977dbc036fc2cca9943cba088
dgh06175 <dgh06175@gmail.com>
2023-10-20 14:45:08 +0900

    1020 실습 코드

--- Cpp_lecture/prac2.cpp
+++ Cpp_lecture/prac2.cpp
@@ -673,6 +673,85 @@ int EX1016_2()
     return 0;
 }
 
+class CIRCLE
+{
+    int radius;
+
+public:
+    CIRCLE() { radius = 0; }
+    CIRCLE(int r) { radius = r; }
+    int getRadius() { return radius; }
+    void setRadius(int r) { radius = r; }
+    float getArea() { return 3.14 * radius * radius; }
+    ~CIRCLE() { radius = 0; }
+};
+
+void EX1020_1()
+{
+    CIRCLE cir[3] = {CIRCLE(10), CIRCLE(20), CIRCLE(30)};
+    cir[0].setRadius(100);
+    cir[1].setRadius(200);
+    cir[2].setRadius(300);
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "면적: " << cir[i].getArea() << endl;
+    }
+
+    CIRCLE *p;
+    p = cir;
+    p++;
+    p->setRadius(-100);
+    (*p).setRadius(-500);
+
+    p = cir;
+    p[0].setRadius(-400);
+    p[1].setRadius(-400);
+    p[2].setRadius(-400);
+    cout << (p + 2)->getArea() << endl;
+}
+
+class Color
+{
+    int red, green, blue;
+
+public:
+    Color() { red = green = blue = 0; }
+    Color(int r, int g, int b)
+    {
+        red = r;
+        green = g;
+        blue = b;
+    }
+    void setColor(int r, int g, int b)
+    {
+        red = r;
+        green = g;
+        blue = b;
+    }
+    void show() { cout << red << ' ' << green << ' ' << blue << endl; }
+};
+
+void EX1020_2()
+{
+    Color screenColor(255, 0, 0); // 빨간색의 screenColor 객체 생성
+    Color *p;                     // Color 타입의 포인터 변수 p 선언
+    p = &screenColor;             // (1) p가 screenColor의 주소를 가지도록 코드 작성
+    p->show();                    // (2) p와 show()를 이용하여 screenColor 색 출력
+    Color colors[3];              // (3) Color의 일차원 배열 colors 선언. 원소는 3개
+    p = colors;                   // (4) p가 colors 배열을 가리키도록 코드 작성
+    // (5) p와 setColor()를 이용하여 colors[0], colors[1], colors[2]가
+    // 각각 빨강, 초록, 파랑색을 가지도록 코드 작성
+    (p)->setColor(255, 0, 0);
+    (p + 1)->setColor(0, 255, 0);
+    (p + 2)->setColor(0, 0, 255);
+    // (6) p와 show()를 이용하여 colors 배열의 모든 객체의 색 출력. for 문 이용
+    for (int i = 0; i < sizeof(colors) / sizeof(Color); i++)
+    {
+        colors[i].show();
+    }
+}
+
 int main()
 {
     // EX_1();
@@ -699,5 +778,7 @@ int main()
     // EX0925_1();
     // EX0925_2();
     // EX1016();
-    EX1016_2();
+    // EX1016_2();
+    // EX1020_1();
+    EX1020_2();
 }
